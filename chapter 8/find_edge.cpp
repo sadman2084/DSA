bbb387731933a97ed76e3e18a864b132537f09f8
Sadmankabir2084 <123572457+sadman2084@users.noreply.github.com>
2024-11-05 06:52:58 +0600

    Update find_edge.cpp

--- chapter 8/find_edge.cpp
+++ chapter 8/find_edge.cpp
@@ -6,32 +6,53 @@ int adj[V][V] = {0};
 
 void addEdge(int src, int dest)
 {
-    adj[src][dest] = 1;
-    adj[dest][src] = 1;
+    if (src >= 0 && src < V && dest >= 0 && dest < V)  // Index range check
+    {
+        adj[src][dest] = 1;
+        adj[dest][src] = 1;
+    }
+    else
+    {
+        cout << "Error: Invalid edge between " << src << " and " << dest << endl;
+    }
 }
 
 int findEdge(int src, int dest)
 {
-    if (adj[src][dest] == 1)
+    if (src >= 0 && src < V && dest >= 0 && dest < V) // Range check for `findEdge`
     {
-        cout << "found";
-        return 0;
+        if (adj[src][dest] == 1)
+        {
+            cout << "found" << endl;
+            return 0;
+        }
+        else
+        {
+            cout << "not found" << endl;
+            return -1;
+        }
     }
-    return -1;
+    else
+    {
+        cout << "Error: Invalid nodes " << src << " or " << dest << " are out of range." << endl;
+        return -1;
+    }
 }
+
 int main()
 {
-
     addEdge(0, 1);
     addEdge(0, 2);
     addEdge(1, 3);
     addEdge(2, 4);
 
-    int src = 1, dest = 3;
+    int src = 1, dest = 9;
     int c = findEdge(src, dest);
 
     if (c == -1)
     {
-        cout << "not found";
+        cout << "not found" << endl;
     }
+
+    return 0;
 }
